846c3c63d35f889de2d6e1076474e6f22f28db7f
Okoniewitz <jawgrzyn@gmail.com>
2025-01-10 13:10:42 +0100

    logika DB

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox_login = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.label_login = new System.Windows.Forms.Label();
+            this.label_password = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_login
+            // 
+            this.textBox_login.Location = new System.Drawing.Point(311, 105);
+            this.textBox_login.Name = "textBox_login";
+            this.textBox_login.Size = new System.Drawing.Size(159, 20);
+            this.textBox_login.TabIndex = 0;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(311, 144);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(159, 20);
+            this.textBox_password.TabIndex = 1;
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(311, 179);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(159, 23);
+            this.button_connect.TabIndex = 2;
+            this.button_connect.Text = "Połącz z bazą";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // label_login
+            // 
+            this.label_login.AutoSize = true;
+            this.label_login.Location = new System.Drawing.Point(308, 89);
+            this.label_login.Name = "label_login";
+            this.label_login.Size = new System.Drawing.Size(33, 13);
+            this.label_login.TabIndex = 3;
+            this.label_login.Text = "Login";
+            // 
+            // label_password
+            // 
+            this.label_password.AutoSize = true;
+            this.label_password.Location = new System.Drawing.Point(308, 128);
+            this.label_password.Name = "label_password";
+            this.label_password.Size = new System.Drawing.Size(36, 13);
+            this.label_password.TabIndex = 4;
+            this.label_password.Text = "Hasło";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 303);
+            this.Controls.Add(this.label_password);
+            this.Controls.Add(this.label_login);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.textBox_login);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_login;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.Label label_login;
+        private System.Windows.Forms.Label label_password;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -4,17 +4,66 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private string connectionString = "Server=193.106.130.55;Database=Project;";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private System.Data.DataTable Query_DB(string query)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                    //dataGridView1.DataSource = dataTable; // Wyświetlanie danych w DataGridView
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Błąd podczas pobierania danych: {ex.Message}");
+                    return null;
+                }
+            }
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            connectionString+= $"Uid = {textBox_login}; Pwd = {textBox_password}";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Połączenie nawiązane pomyślnie!");
+                    label_login.Visible = false;
+                    label_password.Visible = false;
+                    textBox_password.Visible=false;
+                    textBox_login.Visible=false;
+                    button_connect.Visible=false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Błąd połączenia: {ex.Message}");
+                }
+            }
+
         }
     }
 }
